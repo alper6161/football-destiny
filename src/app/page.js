f04f1a6eae0b15a5a7c072f82c0bc5d241f99f6e
Alperen Ustaömer <alperen.ustaomer61@gmail.com>
2024-07-29 20:47:12 +0300

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -16,27 +12,13 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            ALPEREN & BURAK
           </a>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.center} style={{fontWeight: 'bold'}}>
+        ALPEREN & BURAK
       </div>
 
       <div className={styles.grid}>

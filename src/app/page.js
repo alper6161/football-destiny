7692789ad9fdf022f31e63f0a4b8fc1675a29f0e
alperenu <alperen.ustaomer61@gmail.com>
2024-08-26 21:50:08 +0300

    fix small screen problem

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import LeagueTable from "@/components/LeagueTable";
 export default async function Home() {
 
   return (
-      <main style={{}} className="centered page">
+      <main className="centered page">
           <div style={{display: 'grid', gridTemplateColumns: 'auto auto auto', height: '100%', flex: 1, columnGap: '.5rem', rowGap: '.5rem', padding: '0 1rem'}}>
               <div className="widget">
                   <WeekCard />
